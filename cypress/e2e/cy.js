c1da0ca9cc2e978bc238e1338dfe21b536021551
umesh21620008 <umesh.21620008@gmail.com>
2024-05-15 12:29:16 +0530

    Create cy.js

---
+++ cypress/e2e/cy.js
@@ -0,0 +1,11 @@
+describe('Hello World API', () => {
+  it('should return Hello, World!', () => {
+    // Replace with your actual API endpoint
+    const apiUrl = 'http://localhost:3000/';
+
+   
+      // Check that the response body is "Hello, World!"
+      expect(response.body).to.eq('hello');
+    });
+  });
+});
